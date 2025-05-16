a99728acffe87ef4a2d6285f00d4f458ed711cab
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-05-16 20:31:04 +0800

    feat: make site title clickable to navigate to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,12 +22,17 @@ export default function Header() {
     localStorage.removeItem("accessToken");
     navigate("/signin");
   };
+
+  const handleSiteTitleClick = () => {
+    navigate("/");
+  };
+
   return (
     <AppShell.Header>
       <Group h="100%" px="md">
         <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         <Group justify="space-between" style={{ flex: 1 }}>
-          <Group>
+          <Group onClick={handleSiteTitleClick} style={{ cursor: "pointer" }}>
             <MirovaLogo
               style={{ width: "36px", height: "auto" }}
               fill="black"
